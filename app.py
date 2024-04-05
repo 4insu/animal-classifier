4001fa35b6246e838af78a1a0b27c0792789e508
4insu <supriyo.ain.02@gmail.com>
2024-04-06 01:19:22 +0530

    Flask Integration

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-import streamlit as st
+# import streamlit as st
+# import os
+# import cv2
+# import numpy as np
+# from tensorflow.keras.models import load_model
+# from src.utils import animal_classes, load_trained_model, predict_image_class_from_bytes
+
+# # Paths and model loading
+# base_path = os.getcwd()
+# model_folder = 'models'
+# model_filename = 'animal_classification.h5'
+
+# model = load_trained_model(base_path, model_folder, model_filename)
+
+# # Streamlit app
+# st.title('Animal Classifier')
+# st.write("Upload an image and let's predict its class!")
+
+# uploaded_file = st.file_uploader("Choose an image...", type = ["jpg", "jpeg", "png"])
+
+# if uploaded_file is not None:
+#     prediction = predict_image_class_from_bytes(model, uploaded_file)
+#     st.markdown(f'<p style="font-size:30px; color:#ffff00; text-align:center;"><b>This is a {prediction}!</b></p>', 
+#                 unsafe_allow_html = True)
+#     st.image(uploaded_file, caption = 'Uploaded Image', use_column_width = True)
+
+
 import os
-import cv2
+import os
 import numpy as np
-from tensorflow.keras.models import load_model
-from src.utils import animal_classes, load_trained_model, predict_image_class_from_bytes
+from flask import Flask, render_template, request, jsonify
+from src.utils import load_trained_model, predict_image_class_from_bytes
+
+app = Flask(__name__)
 
 # Paths and model loading
 base_path = os.getcwd()
@@ -12,14 +40,18 @@ model_filename = 'animal_classification.h5'
 
 model = load_trained_model(base_path, model_folder, model_filename)
 
-# Streamlit app
-st.title('Animal Classifier')
-st.write("Upload an image and let's predict its class!")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-uploaded_file = st.file_uploader("Choose an image...", type = ["jpg", "jpeg", "png"])
+@app.route('/predict', methods=['POST'])
+def predict():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        prediction = predict_image_class_from_bytes(model, uploaded_file)
+        return jsonify({"prediction": prediction})
+    else:
+        return jsonify({"error": "No file uploaded"})
 
-if uploaded_file is not None:
-    prediction = predict_image_class_from_bytes(model, uploaded_file)
-    st.markdown(f'<p style="font-size:30px; color:#ffff00; text-align:center;"><b>This is a {prediction}!</b></p>', 
-                unsafe_allow_html = True)
-    st.image(uploaded_file, caption = 'Uploaded Image', use_column_width = True)
+if __name__ == '__main__':
+    app.run(debug = True)
